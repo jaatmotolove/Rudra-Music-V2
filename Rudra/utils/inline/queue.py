70e8a179082c9a3ea841763a698c9944aafe134b
RUDRA JAAT <136905244+RUDRA-JAAT@users.noreply.github.com>
2023-10-21 04:55:22 +0530

    Update queue.py

--- Rudra/utils/inline/queue.py
+++ Rudra/utils/inline/queue.py
@@ -66,10 +66,10 @@ def queue_back_markup(_, CPLAY):
 def aq_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="🔊", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="🔇", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="⏭️", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="🛑", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
     ]
     return buttons
