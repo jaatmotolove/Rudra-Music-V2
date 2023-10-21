7639da48f99040170434c2a859bf3f0976f110cd
RUDRA JAAT <136905244+RUDRA-JAAT@users.noreply.github.com>
2023-10-22 05:08:37 +0530

    Update play.py

--- Rudra/utils/inline/play.py
+++ Rudra/utils/inline/play.py
@@ -54,17 +54,17 @@ def stream_markup_timer(_, chat_id, played, dur):
         bar = "—————————✦ᴘɪᴋᴜ✦"
     buttons = [
         [
-            InlineKeyboardButton(
-                text=f"{played} {bar} {dur}",
-                callback_data="GetTimer",
-            )
-        ],
-        [
             InlineKeyboardButton(text="🔊", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="🔇", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="🔁", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="⏭️", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="🛑", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],
+        [
+            InlineKeyboardButton(
+                text=f"{played} {bar} {dur}",
+                callback_data="GetTimer",
+            )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
@@ -73,12 +73,6 @@ def stream_markup_timer(_, chat_id, played, dur):
 
 def stream_markup(_, chat_id):
     buttons = [
-        [
-            InlineKeyboardButton(
-                text=f"{played} {bar} {dur}",
-                callback_data="GetTimer",
-            )
-        ],
         [
             InlineKeyboardButton(text="🔊", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="🔇", callback_data=f"ADMIN Pause|{chat_id}"),
