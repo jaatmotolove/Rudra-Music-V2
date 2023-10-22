d06b6aa9128aab97328612d2494c00cefcc558b4
RUDRA JAAT <136905244+RUDRA-JAAT@users.noreply.github.com>
2023-10-22 05:55:20 +0530

    Update play.py

--- Rudra/utils/inline/play.py
+++ Rudra/utils/inline/play.py
@@ -57,7 +57,7 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
                 callback_data="GetTimer",
-            )
+            ), 
         ],
         [
             InlineKeyboardButton(text="🔊", callback_data=f"ADMIN Resume|{chat_id}"),
