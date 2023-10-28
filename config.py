3c96a15b64c247d1d6c7d3ee2334f1cc92378876
RUDRA JAAT <136905244+RUDRA-JAAT@users.noreply.github.com>
2023-10-29 02:02:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -77,21 +77,21 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/b45a28fb8b88fe7368a38.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/b718ee15a89cdc3dbd93a.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/5a86a80721f7394ed8d73.jpg"
+    "PING_IMG_URL", "https://telegra.ph/file/b718ee15a89cdc3dbd93a.jpg"
 )
-PLAYLIST_IMG_URL = "https://telegra.ph/file/5a86a80721f7394ed8d73.jpg"
-STATS_IMG_URL = "https://telegra.ph/file/5a86a80721f7394ed8d73.jpg"
-TELEGRAM_AUDIO_URL = "https://telegra.ph/file/5a86a80721f7394ed8d73.jpg"
-TELEGRAM_VIDEO_URL = "https://telegra.ph/file/5a86a80721f7394ed8d73.jpg"
-STREAM_IMG_URL = "https://telegra.ph/file/5a86a80721f7394ed8d73.jpg"
-SOUNCLOUD_IMG_URL = "https://telegra.ph/file/5a86a80721f7394ed8d73.jpg"
-YOUTUBE_IMG_URL = "https://telegra.ph/file/5a86a80721f7394ed8d73.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/5a86a80721f7394ed8d73.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/5a86a80721f7394ed8d73.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/5a86a80721f7394ed8d73.jpg"
+PLAYLIST_IMG_URL = "https://telegra.ph/file/b718ee15a89cdc3dbd93a.jpg"
+STATS_IMG_URL = "https://telegra.ph/file/b718ee15a89cdc3dbd93a.jpg"
+TELEGRAM_AUDIO_URL = "https://telegra.ph/file/b718ee15a89cdc3dbd93a.jpg"
+TELEGRAM_VIDEO_URL = "https://telegra.ph/file/b718ee15a89cdc3dbd93a.jpg"
+STREAM_IMG_URL = "https://telegra.ph/file/b718ee15a89cdc3dbd93a.jpg"
+SOUNCLOUD_IMG_URL = "https://telegra.ph/file/b718ee15a89cdc3dbd93a.jpg"
+YOUTUBE_IMG_URL = "https://telegra.ph/file/b718ee15a89cdc3dbd93a.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/b718ee15a89cdc3dbd93a.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/b718ee15a89cdc3dbd93a.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/b718ee15a89cdc3dbd93a.jpg"
 
 
 def time_to_seconds(time):
